ddae1a90fdd2b41966597f1105ba9c919a019bbd
mmanuel <bateemma14@gmail.com>
2023-02-21 19:15:51 +0100

    try to resolve error

--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose")
 const jwt = require("jsonwebtoken")
 const bcrypt = require("bcryptjs")
-const Message = require("../models/Message")
 const { generatePassword, getUserId } = require("../generatePassword")
 const UserSchema = new mongoose.Schema({
     name: {
