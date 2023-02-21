e0c304e203d9817bdb962043e97d111141af79e9
mmanuel <bateemma14@gmail.com>
2023-02-21 18:00:28 +0100

    remove upload.js in routes

--- routes/uploads.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const { createMessage } = require("../controller/Upload")
-    // router.route("/post/:id").post(upload)
-router.route("/message/:id").post(createMessage)
-
-module.exports = router
