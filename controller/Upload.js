06fef3de680da854b65a21beb1f963cc0a06e9f4
mmanuel <bateemma14@gmail.com>
2023-03-04 15:21:49 +0100

    hh

--- controller/Upload.js
+++ controller/Upload.js
@@ -6,8 +6,8 @@ module.exports = {
     createMessage: createMessage = async(req, res) => {
         const { id } = req.params
         const message = req.body.message;
-        res.status(200).json({ msg: [] });
-        return
+        // res.status(200).json({ msg: [] });
+        // return
         const isUser = await User.findOne({ _id: id })
         if (!isUser) { throw new BadErrorRequest("user is not found") }
         const msg = await Message.create({
