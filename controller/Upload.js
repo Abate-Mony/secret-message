c4bda09153db12209ae2f304abc0cda665bebc29
mmanuel <bateemma14@gmail.com>
2023-03-04 14:33:42 +0100

    not working

--- controller/Upload.js
+++ controller/Upload.js
@@ -1,27 +1,18 @@
-const { Message } = require("../models")
-const User = require("../models/User")
+const User = require("../models/User");
+const Message = require("../models/Message.js");
 const { BadErrorRequest } = require("../error")
-    // const path = require("path")
-
-
-
-
 
 module.exports = {
-
     createMessage: createMessage = async(req, res) => {
-        // await Message.init()
         const { id } = req.params
         const message = req.body.message
         const isUser = await User.findOne({ _id: id })
-            // console.log(isUser)
         if (!isUser) { throw new BadErrorRequest("user is not found") }
         const msg = await Message.create({
             sentTo: id,
             message
         })
         res.status(200).json({ msg: msg })
-
 
     }
 }
