644476da70565195ad8ab963fb1518730c28a72c
mmanuel <bateemma14@gmail.com>
2023-03-04 14:43:07 +0100

    jst wrk

--- controller/Message.js
+++ controller/Message.js
@@ -1,4 +1,4 @@
-const { Message } = require("../models")
+const Message = require("../models/Message")
 const { BadErrorRequest } = require("../error")
 const getMessages = async(req, res) => {
     const { userId } = req.userInfo
